9ec58fc5664e6fa1b0c4aa0b4a473f8f86a35610
WallisonRib <ribeirosantoswallison@gmail.com>
2023-11-08 20:17:46 -0300

    fix links padding

--- src/assets/components/Links.jsx
+++ src/assets/components/Links.jsx
@@ -8,7 +8,7 @@ function Links() {
             <button type="button" className=" text-white bg-black  hover:bg-gray-900 hover:ring-1 hover:ring-white border-2 rounded-full text-3xl p-1 mr-5 "> <a href="https://github.com/WallisonRib" target="_blank" > <AiFillGithub /> </a> </button>
             <button type="button" className=" text-white bg-black  hover:bg-gray-900 hover:ring-1 hover:ring-white border-2 rounded-full text-3xl p-1 mr-5 "> <a href="https://www.instagram.com/wallisonrib" target="_blank" ><AiFillInstagram /> </a> </button>
             <button type="button" className=" text-white bg-black  hover:bg-gray-900 hover:ring-1 hover:ring-white border-2 rounded-full text-3xl p-1 mr-5 "> <a href="https://api.whatsapp.com/send/?phone=%2B5538999011905&text&type=phone_number&app_absent=0" target="_blank" ><AiOutlineWhatsApp /> </a> </button>
-            <button type="button" className=" text-white bg-black border-2 hover:bg-gray-900 font-medium rounded-full text-sm px-1 py-1 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" > <a href="https://drive.google.com/file/d/1js8w34ZudWkY5HBpg6pbIMJKNCGiv2Kp/view?usp=sharing" target="_blank"> Baixar curriculo</a> </button>
+            <button type="button" className=" text-white bg-black border-2 hover:bg-gray-900 font-medium rounded-full text-sm px-1 py-1 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" > <a href="https://drive.google.com/file/d/1js8w34ZudWkY5HBpg6pbIMJKNCGiv2Kp/view?usp=sharing" target="_blank"> Download CV</a> </button>
         </div>
     );
 }
