b9d947da48c0dcaf1c2d2e504d143c3682891df0
WallisonRib <ribeirosantoswallison@gmail.com>
2023-11-17 14:33:30 -0300

    new card project

--- src/assets/components/Projects.jsx
+++ src/assets/components/Projects.jsx
@@ -1,6 +1,7 @@
 import Card from "./Card";
 import foto_ml from "../images/preview_clone_ml.png"
 import foto_cep from "../images/preview_buscadordecep.png"
+import foto_auth from "../images/preview_php_auth.png"
 
 function Projects() {
     return (
@@ -10,6 +11,7 @@ function Projects() {
                 <div class="grid grid-cols-1 md:grid-cols-3 gap-10 text-white 2xl:mx-60 m-5">
                     <Card foto={foto_ml} titulo="Clone funcional do ML" texto="Consumo de API e exposição de dados dos produtos " repositorio={"https://github.com/WallisonRib/Clone-Funcional-MercadoLivre"} />
                     <Card foto={foto_cep} titulo="Buscador de Cep" texto="Projeto realizado em React.js com consumo de API viacep e validação de campos para exposição de dados" repositorio={"https://github.com/WallisonRib/BuscadorDeCep"} />
+                    <Card foto={foto_auth} titulo="Cadastro e Login PHP" texto="CRUD feito em php para Cadstro, Login e Consulta de usuários" repositorio={"https://github.com/WallisonRib/auth_php"} />
                 </div>
             </div>
         </section>
