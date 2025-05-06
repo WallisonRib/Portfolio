669904d577eefa0612177e6fe0ef997f41eb1f26
WallisonRib <ribeirosantoswallison@gmail.com>
2025-05-05 23:09:49 -0300

    feat: new projects

--- src/assets/components/Projects/Projects.jsx
+++ src/assets/components/Projects/Projects.jsx
@@ -1,8 +1,7 @@
 import Card from "./Card";
 import foto_ml from "../../images/preview_clone_ml.png"
-import foto_cep from "../../images/preview_buscadordecep.png"
-import foto_auth from "../../images/preview_php_auth.png"
 import circuito from "../../images/circuito.png"
+import swagger from "../../images/swagger.png"
 
 
 function Projects() {
@@ -11,7 +10,9 @@ function Projects() {
             <h1 className="font-bold text-3xl md:text-4xl flex py-10 justify-center">Projetos</h1>
             <div className="wrap flex justify-center">
                 <div class="grid grid-cols-1 md:grid-cols-3 gap-10 text-white 2xl:mx-60 m-5">
-                    <Card foto={circuito} titulo="Site do Circuito de Conhecimentos" texto="Site criado em Laravel, PHP e MySQL para gerenciamento e exposição de conteudos referentes ao CDC" repositorio={"https://github.com/WallisonRib/Clone-Funcional-MercadoLivre"} />
+                    <Card foto={circuito} titulo="Site do Circuito de Conhecimentos" texto="Site criado em Laravel, PHP e MySQL para gerenciamento e exposição de conteudos referentes ao CDC" repositorio={"https://github.com/WallisonRib/CircuitoDeConhecimentos"} />
+                    <Card foto={swagger} titulo="API Node + Swagger + Postgree" texto="API criada com Node, Swagger e Postgree para alimentação de dados para o site Econolivros, feito em React" repositorio={"https://github.com/WallisonRib/SwaggerAPI_PostgreeSQL"} />
+
                     <Card foto={foto_ml} titulo="Clone funcional do ML" texto="Consumo de API e exposição de dados dos produtos " repositorio={"https://github.com/WallisonRib/Clone-Funcional-MercadoLivre"} />
                 </div>
             </div>
